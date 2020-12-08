6b54e96f879f9339f7f994d112e54c0e184a2b92
palit-shreshtha <74540358+palit-shreshtha@users.noreply.github.com>
2020-12-08 12:11:19 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var fixedRect, movingRect;
+
+var ob1, ob2, ob3,ob4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+
+  ob1= createSprite (100,100,50,50);
+  ob1.shapeColor = "green";
+  ob2= createSprite (200,100,50,50);
+  ob2.velocityX=-2;
+  ob2.shapeColor = "green";
+  ob3= createSprite (300,100,50,50);
+  ob4= createSprite (1000,100,50,50);
+  ob3.shapeColor = "yellow";
+  ob4.shapeColor = "blue";
+  ob3.velocityX=3;
+  ob4.velocityX=-3;
+
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+if (  isTouching(ob2, ob1)){
+
+  movingRect.shapeColor = "red";
+  fixedRect.shapeColor = "red";
+
+
+} else {
+
+
+  movingRect.shapeColor = "green";
+  fixedRect.shapeColor = "green";
+}
+
+bounceOff(ob3,ob4);
+
+
+  drawSprites();
+}
+
